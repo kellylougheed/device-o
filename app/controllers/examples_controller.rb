4c65f5ac25d2700de8caa03ff1704d9a1388f119
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-30 22:44:26 +0000

    Add dropdown menu search

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,12 +1,21 @@
 class ExamplesController < ApplicationController
   
   def index
-    @example = Example.first
+    if params[:example]
+      @examples = Example.where(device: params[:example][:device].downcase)
+      @query = @examples[0].device
+    else
+      @examples = Example.all
+    end
+  end
+
+  def random
+    @example = Example.order("RANDOM()").first
   end
 
   def create
     @example = Example.new(example_params)
-    if @new_example.invalid?
+    if @example.invalid?
       flash[:error] = 'Please enter a valid example longer than 10 characters with a device selected.'
       redirect_to root_path
     else
