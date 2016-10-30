8000938ca5c872f17563e8f1b509e89085b649ff
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-30 19:48:15 +0000

    Add form to add new examples

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -4,4 +4,22 @@ class ExamplesController < ApplicationController
     @example = Example.first
   end
 
+  def create
+    @example = Example.new(example_params)
+    if @new_example.invalid?
+      flash[:error] = 'Please enter a valid example longer than 10 characters with a device selected.'
+      redirect_to root_path
+    else
+      @example.save
+      redirect_to root_path
+      flash[:success] = 'Thanks for adding an example!'
+    end
+  end
+
+  private
+
+  def example_params
+    params.require(:example).permit(:text, :device)
+  end
+
 end
--- app/models/example.rb
+++ app/models/example.rb
@@ -1,2 +1,4 @@
 class Example < ActiveRecord::Base
+  validates :text, presence: true, length: { minimum: 10 }
+  validates :device, presence: true
 end
